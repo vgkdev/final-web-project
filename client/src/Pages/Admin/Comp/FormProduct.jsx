dac4fd17cd81e8f68491579d76bca1fa0aba7025
Khuong Mai (Lenovo SLim7 ProX) <maikhuong2003@gmail.com>
2024-01-28 18:07:01 +0700

    fix error

--- client/src/Pages/Admin/Comp/FormProduct.jsx
+++ client/src/Pages/Admin/Comp/FormProduct.jsx
@@ -1,3 +1,4 @@
+import noImage from "../../../assets/images/noimage.jpg";
 import React, { useEffect, useState } from "react";
 import {
   Button,
@@ -5,10 +6,9 @@ import {
   FormLabel,
   Input,
   Stack,
-  Select, StackItem,
+  Select, Image
 } from "@chakra-ui/react";
-
-import noImage from "../../..//assets/images/noimage.jpg";
+import { Images } from "../../Home/Data";
 
 const FormProduct = (props) => {
   const {
@@ -23,7 +23,7 @@ const FormProduct = (props) => {
   const [productName, setProductName] = useState("");
   const [quantity, setQuantity] = useState("");
   const [description, setDescription] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+  const [imageUrl, setImageUrl] = useState(null);
   const [price, setPrice] = useState("");
   // console.log("check product form: ", product);
 
@@ -70,112 +70,94 @@ const FormProduct = (props) => {
     }
   };
 
-  const handleFileChange = (event) => {
-    const selectedFile = event.target.files[0];
-
-    if (selectedFile) {
-      const reader = new FileReader();
-
-      reader.onloadend = () => {
-        const imageUrl = reader.result;
-        // Do something with the imageUrl, such as updating the state
-        setImageUrl(imageUrl);
-      };
-
-      // Read the contents of the selected file as a data URL
-      reader.readAsDataURL(selectedFile);
-    }
-  };
-
   //   console.log("check data in form: ", user);
   return (
-    <form>
-      <Stack spacing={3}>
-        <FormControl display={type === "Update_image" ? "none" : ""}>
-          <FormLabel>Tên sản phẩm</FormLabel>
-          <Input
-              type="text"
-              value={productName}
-              onChange={(event) => setProductName(event.target.value)}
-              placeholder="Tên sản phẩm"
-              disabled={type === "Detail"}
-          />
-        </FormControl>
+      <form>
+        <Stack spacing={3}>
+          <FormControl display={type === "Update_image" ? "none" : ""}>
+            <FormLabel>Tên sản phẩm</FormLabel>
+            <Input
+                type="text"
+                value={productName}
+                onChange={(event) => setProductName(event.target.value)}
+                placeholder="Tên sản phẩm"
+                disabled={type === "Detail"}
+            />
+          </FormControl>
 
-        <FormControl display={type === "Update_image" ? "none" : ""}>
-          <FormLabel>Số lượng</FormLabel>
-          <Input
-              type="text"
-              value={quantity}
-              onChange={(event) => setQuantity(event.target.value)}
-              placeholder="Số lượng"
-              disabled={type === "Detail"}
-          />
-        </FormControl>
+          <FormControl display={type === "Update_image" ? "none" : ""}>
+            <FormLabel>Số lượng</FormLabel>
+            <Input
+                type="text"
+                value={quantity}
+                onChange={(event) => setQuantity(event.target.value)}
+                placeholder="Số lượng"
+                disabled={type === "Detail"}
+            />
+          </FormControl>
 
-        <FormControl display={type === "Update_image" ? "none" : ""}>
-          <FormLabel>Giá</FormLabel>
-          <Input
-              type="text"
-              value={price}
-              onChange={(event) => setPrice(event.target.value)}
-              placeholder="Giá"
-              disabled={type === "Detail"}
-          />
-        </FormControl>
+          <FormControl display={type === "Update_image" ? "none" : ""}>
+            <FormLabel>Giá</FormLabel>
+            <Input
+                type="text"
+                value={price}
+                onChange={(event) => setPrice(event.target.value)}
+                placeholder="Giá"
+                disabled={type === "Detail"}
+            />
+          </FormControl>
 
-        <FormControl display={type === "Update_image" ? "none" : ""}>
-          <FormLabel>Mô tả</FormLabel>
-          <Input
-              type="text"
-              value={description}
-              onChange={(event) => setDescription(event.target.value)}
-              placeholder="Mô tả"
-              disabled={type === "Detail"}
-          />
-        </FormControl>
+          <FormControl display={type === "Update_image" ? "none" : ""}>
+            <FormLabel>Mô tả</FormLabel>
+            <Input
+                type="text"
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+                placeholder="Mô tả"
+                disabled={type === "Detail"}
+            />
+          </FormControl>
 
-        <FormControl display={type === "Update_image" ? "none" : ""}>
-          <FormLabel>Tên danh mục</FormLabel>
-          <Select
-              name="categoryName"
-              value={categoryId}
-              onChange={(event) => setCategoryId(event.target.value)}
-          >
-            <option value={""}>---Tên danh mục---</option>
-            {categories.map((value) => (
-                <option key={value.id} value={value.id}>
-                  {value.categoryName}
-                </option>
-            ))}
-          </Select>
-        </FormControl>
+          <FormControl display={type === "Update_image" ? "none" : ""}>
+            <FormLabel>Tên danh mục</FormLabel>
+            <Select
+                name="categoryName"
+                value={categoryId}
+                onChange={(event) => setCategoryId(event.target.value)}
+            >
+              <option value={""}>---Tên danh mục---</option>
+              {categories.map((value) => (
+                  <option key={value.id} value={value.id}>
+                    {value.categoryName}
+                  </option>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl display={type === "Update" ? "none" : ""}>
+            <img
+                src={imageUrl ? URL.createObjectURL(imageUrl) : noImage}
+                alt="Product Image"
+                style={{
+                  margin: "10px auto 10px auto",
+                  width: "20rem",
+                  height: "20rem"
+                }}
+            />
+            <FormLabel>Ảnh</FormLabel>
+            <Input
+                type="file"
+                onChange={(event) => {setImageUrl(event.target.files[0])}}
+                disabled={type === "Detail"}
+            />
+          </FormControl>
 
-        <img
-            src={imageUrl ? imageUrl : noImage}
-            alt="Product Image"
-            style={{
-              margin:"10px auto 10px auto",
-              width:"20rem",
-              height:"20rem"
-            }}
-        />
-
-        <FormControl display={type === "Update" ? "none" : ""}>
-          <FormLabel>Ảnh</FormLabel>
-          <Input
-              type="file"
-              onChange={handleFileChange}
-              disabled={type === "Detail"}
-          />
-        </FormControl>
-        {type !== "Detail" && (
-            <Button colorScheme="teal" onClick={handleOnClickSubmit}>
-              Submit
-            </Button>
-        )}
-      </Stack>
-    </form>
+          {type !== "Detail" && (
+              <Button colorScheme="teal" onClick={handleOnClickSubmit}>
+                Submit
+              </Button>
+          )}
+        </Stack>
+      </form>
   );
 };
 
